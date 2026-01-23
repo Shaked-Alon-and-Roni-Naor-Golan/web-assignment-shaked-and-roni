64f78815947c97dabcfd45fcde5096011252f04d
רוני נאור גולן <roninaorgo@gmail.com>
2026-01-23 18:57:10 +0200

    Changed posts_model to be typescript based

--- models/posts_model.js
+++ models/posts_model.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
+import { Post } from "../dtos/post";
 
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema<Post>({
   title: {
     type: String,
     required: true,
   },
   sender: {
-    type: mongoose.Types.ObjectId,
+    type: mongoose.Types.ObjectId, // TODO: change to User type when User model is created - add ref to "users"
     required: true,
   },
   content: String,
 });
 
-module.exports = mongoose.model("posts", postSchema);
+export const PostModel = mongoose.model<Post>("posts", postSchema);
