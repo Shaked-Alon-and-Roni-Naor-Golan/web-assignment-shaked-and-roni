b171223ecd1ffe642b1cc7541e78a4e55cf09973
Roni NG <46071682+RoniNaGo@users.noreply.github.com>
2026-01-24 15:36:04 +0200

    Add jwt authentication (#6)
    * Added RefershToken dto
    
    * Added refresh_token_model
    
    * Added generate_access_token and generate_refresh_token to utils/auth directory
    
    * Added refresh_token_service to services
    
    * Added auth_middleware for authentication
    
    * Added authentication controller for authentication
    
    * Added authentication route
    
    * Added auth route to app.ts
    
    * Modified modules installed package.json and request.rest
    
    * Finished

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
+import { authenticateToken } from "./middlewares/auth_middleware";
+
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -14,12 +16,18 @@ db.once("open", () => console.log("Connected to database successfully"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const postsRouter = require("./routes/posts_route");
-const commentsRouter = require("./routes/comments_route");
-const usersRouter = require("./routes/users_route");
+app.use(authenticateToken);
 
+const authRouter = require("./routes/auth_route");
+app.use("/auth", authRouter);
+
+const postsRouter = require("./routes/posts_route");
 app.use("/posts", postsRouter);
+
+const commentsRouter = require("./routes/comments_route");
 app.use("/comments", commentsRouter);
+
+const usersRouter = require("./routes/users_route");
 app.use("/users", usersRouter);
 
 app.listen(process.env.PORT, () => {
