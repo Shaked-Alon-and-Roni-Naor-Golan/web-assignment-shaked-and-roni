5a027ae392dd1e9929669363412171d6af7f2486
Roni NG <46071682+RoniNaGo@users.noreply.github.com>
2026-01-27 23:59:59 +0200

    Add users and auth tests (#13)
    * Added user_to_token_data
    
    * changed some things in logout
    
    * Added all auth tests
    
    * Added user.test.ts
    
    * Finished user.test.ts
    
    * Added test coverage to project
    
    * Added some more tests to auth.test.ts
    
    * Removed example.test
    
    * Reached to 100% tests coverage for auth and user modules
    
    * Nothing changed
    
    * Removed coverage directory
    
    * Changes made in the tests of auth and users
    
    ---------
    
    Co-authored-by: Shaked Alon <105640966+shakedalon5@users.noreply.github.com>

--- controllers/auth_controller.ts
+++ controllers/auth_controller.ts
@@ -1,29 +1,29 @@
-import { UserModel } from "../models/user_model";
-import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { User } from "../dtos/user";
-import { createNewUser } from "../services/user_service";
+import { Request, RequestHandler, Response } from "express";
+import { UserModel } from "../models/user_model";
+import { createNewUser, findUserById } from "../services/user_service";
 import { generateAccessToken } from "../utils/auth/generate_access_token";
 import { generateRefreshToken } from "../utils/auth/generate_refresh_token";
-import {
-  createRefreshToken,
-  deleteRefreshToken,
-} from "../services/refresh_token_service";
-const dotenv = require("dotenv");
-dotenv.config();
-
-let refreshTokens: string[] = [];
+import { userToTokenData } from "../utils/auth/user_to_token_data";
 
 export const register = async (req: Request, res: Response) => {
   const user: User = req.body;
   try {
-    const userExistsCheck = await UserModel.findOne({ email: user.email });
-    if (userExistsCheck) {
+    const currentUser = await UserModel.findOne({ email: user.email });
+
+    if (currentUser) {
       throw Error("User already exists");
     }
 
-    await createNewUser(user);
+    const hashedPassword = await bcrypt.hash(
+      user.password,
+      await bcrypt.genSalt()
+    );
+    await createNewUser({ ...user, password: hashedPassword });
 
-    res.status(201).send("User registered successfully");
+    res.send("User completed registration");
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -32,44 +32,134 @@ export const register = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
-    const user = await UserModel.findOne({ email, password });
+    const user = await UserModel.findOne({ email });
+    if (user == null) {
+      throw Error("Invalid Credentials");
+    }
 
-    if (!user) {
-      throw Error("Invalid Cardentials");
+    const doesPasswordsMatch = await bcrypt.compare(password, user.password);
+    if (!doesPasswordsMatch) {
+      throw Error("Invalid Credentials");
     }
 
     const accessToken = generateAccessToken(
-      convertUserToPlain(user),
+      userToTokenData(user),
       process.env.ACCESS_TOKEN_SECRET,
-      "5m"
+      process.env.ACCESS_TOKEN_EXPIRATION
     );
+
     const refreshToken = generateRefreshToken(
-      convertUserToPlain(user),
+      userToTokenData(user),
       process.env.REFRESH_TOKEN_SECRET,
-      "1d"
+      process.env.REFRESH_TOKEN_EXPIRATION
     );
 
-    await createRefreshToken(refreshToken);
+    user.refreshTokens = user.refreshTokens
+      ? [...user.refreshTokens, refreshToken]
+      : [refreshToken];
+    await user.save();
 
-    res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true });
-    res.status(200).send(accessToken);
+    res.send({ accessToken, refreshToken });
   } catch (err) {
     res.status(500).send(err.message);
   }
 };
 
-export const logout = async (req: Request, res: Response) => {
-  const { refreshToken } = req.cookies;
-  if (!refreshToken) throw Error("No refresh token provided");
-
-  await deleteRefreshToken(refreshToken);
-
-  res.clearCookie("refreshToken");
-  res.status(200).send("Logged out successfully");
+const getToken = (authorizationHeader: string) => {
+  return authorizationHeader?.split(" ")?.[1];
 };
 
-const convertUserToPlain = (user: User): Pick<User, "_id"> => {
-  return {
-    _id: user._id.toString(),
-  };
+export const logout: RequestHandler = (req, res) => {
+  const refreshToken = getToken(req.headers.authorization as string);
+
+  if (!refreshToken) {
+    res.status(401).send("Refresh token is not provided");
+    return;
+  }
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET as string,
+    async (err, userInfo: any) => {
+      if (err) {
+        res.status(403).send("Unauthorized");
+        return;
+      }
+
+      try {
+        const user = await findUserById(userInfo._id);
+        if (!user) {
+          res.status(403).send("Unauthorized");
+          return;
+        }
+
+        if (!user.refreshTokens.includes(refreshToken)) {
+          user.refreshTokens = [];
+          await user.save();
+          res.status(403).send("Unauthorized");
+          return;
+        }
+
+        user.refreshTokens = user.refreshTokens.filter(t => t !== refreshToken);
+        await user.save();
+        res.send("Logged out");
+      } catch (e: any) {
+        res.status(403).send(e.message);
+      }
+    }
+  );
+};
+
+export const refreshToken: RequestHandler = async (req, res) => {
+  const token = getToken(req.headers.authorization as string);
+
+  if (!token) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  jwt.verify(
+    token,
+    process.env.REFRESH_TOKEN_SECRET as string,
+    async (err, userInfo: any) => {
+      if (err) {
+        res.status(403).send("Unauthorized");
+        return;
+      }
+
+      try {
+        const user = await UserModel.findById(userInfo._id);
+        if (!user) {
+          res.status(403).send("Unauthorized");
+          return;
+        }
+
+        if (!user.refreshTokens.includes(token)) {
+          user.refreshTokens = [];
+          await user.save();
+          res.status(403).send("Unauthorized");
+          return;
+        }
+
+        const newAccessToken = generateAccessToken(
+          userToTokenData(user),
+          process.env.ACCESS_TOKEN_SECRET as string,
+          process.env.ACCESS_TOKEN_EXPIRATION as string
+        );
+
+        const newRefreshToken = generateRefreshToken(
+          userToTokenData(user),
+          process.env.REFRESH_TOKEN_SECRET as string,
+          process.env.REFRESH_TOKEN_EXPIRATION as string
+        );
+
+        user.refreshTokens[user.refreshTokens.indexOf(token)] = newRefreshToken;
+        await user.save();
+
+        res.send({ accessToken: newAccessToken, refreshToken: newRefreshToken });
+      } catch (e: any) {
+        res.status(403).send(e.message);
+      }
+    }
+  );
 };
