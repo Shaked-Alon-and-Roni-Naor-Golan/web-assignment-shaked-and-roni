82f8c9f029646794695c7fb6491a41e084fbd293
RoniNaGo <roninaorgo@gmail.com>
2026-02-20 12:04:30 +0200

    updated navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,23 @@
-import { Link } from "react-router-dom";
+import { authLogout } from "../services/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
+import { enqueueSnackbar } from "notistack";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { setUser } = useUserContext() ?? {};
+
+  const handleLogout = async () => {
+    try {
+      await authLogout();
+      setUser?.(null);
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+      enqueueSnackbar("Failed to logout", { variant: "error" });
+    }
+  };
+
   return (
     <nav
       className="navbar navbar-expand-lg navbar-dark bg-dark"
@@ -10,11 +27,11 @@ const Navbar = () => {
         <Link className="navbar-brand" to="/">
           <img
             src="/logo.png"
-            alt="Movieland Logo"
+            alt="JustEat Logo"
             className="logo m-2 rounded-circle"
             style={{ width: "30px", height: "30px" }}
           />
-          MovieLand
+          JustEat
         </Link>
         <button
           className="navbar-toggler"
@@ -27,6 +44,39 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <div className="nav-link">|</div>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile" className="nav-link">
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <div className="nav-link">|</div>
+            </li>
+            <li className="nav-item">
+              <Link to="/add-post" className="nav-link">
+                Add Resturant
+              </Link>
+            </li>
+            <li className="nav-item">
+              <div className="nav-link">|</div>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn btn-link" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
