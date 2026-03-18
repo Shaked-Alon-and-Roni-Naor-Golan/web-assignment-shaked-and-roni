da4c696a9820f3b0864e164671aad0bc97b1ed57
Shaked Canani <shakedalon5202411@gmail.com>
2026-03-18 02:17:17 +0200

    edit server.ts

--- server/server.ts
+++ server/server.ts
@@ -4,11 +4,28 @@ const dotenv = require("dotenv");
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 import appPromise from "./src/app";
+import https from "https";
+import fs from "fs";
+import http from "http";
 
-appPromise.then((app) =>
-  app.listen(process.env.PORT, () => {
-    console.log(
-      `Example app listening at http://localhost:${process.env.PORT}`
-    );
-  })
-);
+// appPromise.then((app) =>
+//   app.listen(process.env.PORT, () => {
+//     console.log(
+//       `Example app listening at http://localhost:${process.env.PORT}`
+//     );
+//   })
+// );
+
+appPromise.then((app) => {
+  if (process.env.NODE_ENV !== "production") {
+    console.log("development");
+    http.createServer(app).listen(process.env.PORT);
+  } else {
+    console.log("PRODUCTION");
+    const option2 = {
+      key: fs.readFileSync('../../client-key.pem'),
+      cert: fs.readFileSync('../../client-cert.pem'),
+    };
+    https.createServer(option2, app).listen(process.env.HTTPS_PORT);
+  }
+});
