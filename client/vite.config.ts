e8d5fed0dd0094f0a406a4df7b6fc7d50e41050e
Shaked Canani <shakedalon5202411@gmail.com>
2026-03-19 19:31:51 +0200

    edit vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'node:fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "localhost",
+    host: "0.0.0.0",
     port: 5173,
     strictPort: true,
     headers: {
@@ -13,4 +14,12 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "unsafe-none",
     },
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 443,
+    https: {
+      key: fs.readFileSync('../../client-key.pem'),
+      cert: fs.readFileSync('../../client-cert.pem'),
+    }
+  }
 })
